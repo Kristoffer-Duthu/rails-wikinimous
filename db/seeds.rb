e3a595d7b7f0af2fb45e33766bf7a4f3b8c84539
Kristoffer Duthu <kristoffer.duthu@gmail.com>
2021-05-27 21:52:05 +0800

    created the model, seed, routes, controller and initial css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  art = Article.create(
+    title: Faker::JapaneseMedia::SwordArtOnline.real_name,
+    content: Faker::TvShows::BojackHorseman.tongue_twister
+  )
+  art.save
+end
